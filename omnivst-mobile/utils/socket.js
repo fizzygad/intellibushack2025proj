1a698b222dd57a42c4954aa8b7b716c274216d11
Joel Foster <janthonyf70@gmail.com>
2025-10-19 09:18:48 -0500

    Revert "Mobilev1.1"
    This reverts commit cddf4173e52a82bfdea7bb9c920e4ac4fd576cfc.

--- omnivst-mobile/utils/socket.js
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-
-// Replace with your backend’s real URL
-export const socket = io("https://your-backend-url.com", {
-  transports: ["websocket"],
-  reconnectionAttempts: 5,
-});
